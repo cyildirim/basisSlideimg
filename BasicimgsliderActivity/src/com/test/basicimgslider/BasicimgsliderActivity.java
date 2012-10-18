5f899dfe85e1b2b271d45ac06b9778671d5eb937
cany <cany@cany-Satellite-A505>
2012-10-18 15:13:21 +0300

    Fix horizontal layout errors

--- BasicimgsliderActivity/src/com/test/basicimgslider/BasicimgsliderActivity.java
+++ BasicimgsliderActivity/src/com/test/basicimgslider/BasicimgsliderActivity.java
@@ -18,31 +18,12 @@ import android.view.View.OnTouchListener;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-public class BasicimgsliderActivity extends Activity implements OnGesturePerformedListener {
+public class BasicimgsliderActivity extends Activity {
 	private GestureLibrary gestureLib;
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        GestureOverlayView gestureOverlayView = new GestureOverlayView(this);
-        View inflate = getLayoutInflater().inflate(R.layout.main,null);
-        gestureOverlayView.addView(inflate);
-        gestureOverlayView.addOnGesturePerformedListener(this);
-        gestureOverlayView.setGestureColor(Color.rgb(0,255,0));
-        gestureOverlayView.setGestureVisible(true);
-        gestureLib = GestureLibraries.fromRawResource(this,R.raw.gestures);
-        if(!gestureLib.load()){
-        	finish();
-        }
-        setContentView(gestureOverlayView);
+        setContentView(R.layout.main);
     }
-	@Override
-	public void onGesturePerformed(GestureOverlayView overlay, Gesture gesture) {
-		ArrayList<Prediction> predictions =gestureLib.recognize(gesture);
-		for(Prediction prediction:predictions){
-			if(prediction.score > 1.0){
-				Toast.makeText(this,prediction.name,Toast.LENGTH_SHORT).show();
-			}
-		}
-		
-	}
+
 }
